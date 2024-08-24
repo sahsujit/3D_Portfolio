bb93273821da5d63066c35fed818ce6e170fdd98
Sujit Sah <129708170+sahsujit@users.noreply.github.com>
2024-08-24 18:48:20 +0545

    Update page.js

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -35,7 +35,7 @@ export default function Home() {
             Sujit 
           </h1>
           <p className="font-light text-foreground text-lg">
-            Meet the wizard behind this portfolio
+
           </p>
         </div>
       </div>
